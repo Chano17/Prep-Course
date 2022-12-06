97269cc308a2f179959fb29ca02ddfdcf5029f6e
Versiones2022* <alexamaya17@yahoo.com.mx>
2022-12-06 15:00:50 -0500

    cambio3

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -83,11 +83,7 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(numero ===10 || numero === 5 ){
-return true;
-  }else {
-  return false;
-}
+ return numero ===10 || numero === 5;
 }
 
 
@@ -125,10 +121,10 @@ function fizzBuzz(numero) {
    if(numero % 3 ===0){
     return "fizz";
    }
-   if(numero %5){
+   if(numero % 5){
     return "buzz";
    }
-    if(numero %15 ===0){
+    if(numero % 15 ===0){
     return "fizzbuzz";
     }
     return numero;
@@ -141,18 +137,33 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  if( num1> 0 && num1 > num2 && num1 > num3){
-    return "Número 1 es mayor y positivo";
-} else if (num1 < 0 || num2 < 0 || num3 < 0){
+  // if(num1> 0 && num1 > num2 && num1 > num3){
+   // return "Número 1 es mayor y positivo";
+//} else if (num1 < 0 || num2 < 0 || num3 < 0){
+//  return "Hay negativos";
+//} else if(num3 > num1 && num3 > num2){
+ // return num3 + 1;
+//}else if (num1 ===0 || num2 ===0 || num3 ===0){
+ // return "Error";
+//}else {
+ //   return false;
+ if(num1 < 0 || num2 < 0 || num3 < 0) {
   return "Hay negativos";
-} else if(num3 > num1 && num3 > num2){
-  return num3 + 1;
-}else if (num1 ===0 || num2 ===0 || num3 ===0){
+}
+else if(num1 === 0 || num2 === 0 || num3 === 0) {
   return "Error";
-}else {
-    return false;
+}
+else if(num1 > 0 && num1 > num2 && num1 > num3) {
+  return "Número 1 es mayor y positivo";
+}
+else if(num3 > num1 && num3 > num2) {
+  return num3 + 1;
+}
+else {
+  return false;
 }
 }
+
 
 function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
